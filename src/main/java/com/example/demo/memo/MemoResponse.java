66df7138be8e8570be2affbd758109f7b80a9360
JSH-631 <newred02@handong.ac.kr>
2025-02-13 16:54:37 +0900

    fix: MemoResponse 수정

--- src/main/java/com/example/demo/memo/MemoResponse.java
+++ src/main/java/com/example/demo/memo/MemoResponse.java
@@ -13,22 +13,12 @@ public class MemoResponse {
     private String bookTitle;
     private String content;
     private String date;
-    private String message;
 
     public static MemoResponse from(MemoDto memoDto) {
         return MemoResponse.builder()
                 .bookTitle(memoDto.getBookTitle())
                 .content(memoDto.getContent())
                 .date(memoDto.getDate())
-                .build();
-    }
-
-    public static MemoResponse from(MemoDto memoDto, String successMessage) {
-        return MemoResponse.builder()
-                .bookTitle(memoDto.getBookTitle())
-                .content(memoDto.getContent())
-                .date(memoDto.getDate())
-                .message(successMessage)
                 .build();
     }
 
